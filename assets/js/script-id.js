3b779cf48df0646e697d15666eaebf78c08daa25
greneboy <greneboy@playground.io>
2025-03-04 14:22:09 +0700

    Localized index.html, added leaflet.js, and small fixes

--- assets/js/script-id.js
+++ assets/js/script-id.js
@@ -1,4 +1,21 @@
-const words = ["Kebenaran terbenarkan", "Memang Indie", "Buruk itu bagus"];
+// Maps
+var map = L.map('map').setView([-7.435167, 109.250249], 9);
+
+var myIcon = L.icon({
+    iconUrl: '../assets/images/logo.png',
+    iconSize: [32, 32],
+    iconAnchor: [16, 16],
+    popupAnchor: [-3, -76],
+});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([-7.435167, 109.250249],{icon:myIcon}).addTo(map);
+
+const words = ["in·\u200Bkon·\u200Bven·\u200Bsi·\u200Bo·\u200Bnal", "Memang·\u200BIndie", "Buruk itu bagus"];
 const shell = [">", "$", "]", "_", "%", "#"];
 
 // Function to get a random word
@@ -15,3 +32,25 @@ function getRandomShell() {
 // Display the random word
 document.getElementById('greetings').textContent = getRandomWord();
 document.getElementById('shell').textContent = getRandomShell();
+
+const basePath = window.location.pathname.startsWith('/port-1/') ? '/port-1/' : '/';
+
+// Asciinema
+const lavaPath = `${basePath}assets/misc/lavat-id.cast`;
+const lavaSkel = document.getElementById('lava-skeleton');
+const lava = document.getElementById('lava');
+
+lava.style.display = 'none';
+
+const player = AsciinemaPlayer.create(lavaPath, lava, {
+  terminalFontFamily: "'IBM Plex Mono', monospace",
+  loop: true,
+  autoPlay: true,
+  controls: false,
+  fit: false,
+});
+
+player.addEventListener('playing', () => {
+  lavaSkel.remove();
+  lava.style.display = 'block';
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+// Maps
+var map = L.map('map').setView([-7.435167, 109.250249], 9);
+
+var myIcon = L.icon({
+    iconUrl: '../assets/images/logo.png',
+    iconSize: [32, 32],
+    iconAnchor: [16, 16],
+    popupAnchor: [-3, -76],
+});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([-7.435167, 109.250249],{icon:myIcon}).addTo(map);
+
 const words = ["un·\u200Bcon·\u200Bven\u200Btion·\u200Bal", "Indie·\u200BIndeed", "Worse is better"];
 const shell = [">", "$", "]", "_", "%", "#"];
 
@@ -16,8 +33,9 @@ function getRandomShell() {
 document.getElementById('greetings').textContent = getRandomWord();
 document.getElementById('shell').textContent = getRandomShell();
 
-//Asciinema
 const basePath = window.location.pathname.startsWith('/port-1/') ? '/port-1/' : '/';
+
+// Asciinema
 const lavaPath = `${basePath}assets/misc/lavat.cast`;
 const lavaSkel = document.getElementById('lava-skeleton');
 const lava = document.getElementById('lava');
