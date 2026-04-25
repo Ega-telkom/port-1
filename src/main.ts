07e6b8f11c16df14da24c46be3afcdcfb565bf16
Ega-telkom <541241041@student.smktelkom-pwt.sch.id>
2026-04-25 15:16:45 +0700

    delete: leaflet

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import App from './App.vue'
 import router from './router'
 import { i18n } from './i18n'
 import './style.css'
-import 'leaflet/dist/leaflet.css'
-import '@ibm/plex-mono/css/ibm-plex-mono-all.css'
-import 'material-icons/iconfont/material-icons.css';
 
 createApp(App)
   .use(router)
