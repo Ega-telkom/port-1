081a91d408e0885ceea9bb91517c119bc8695e06
Ega-telkom <541241041@student.smktelkom-pwt.sch.id>
2026-04-25 15:12:54 +0700

    feat: cache assets

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts">
+import { onMounted } from "vue";
 import Navbar from "@components/Navbar.vue";
 import DotMatrixPaper from "@components/DotMatrixPaper.vue";
 import LocaleSwitch from "@components/LocaleSwitch.vue";
+
+onMounted(() => {
+    [
+        "/images/port-1.webp",
+        "/images/fundivest.webp",
+        "/images/biblio.webp",
+        "/images/jalurku.webp",
+        "/images/smilebox.webp",
+        "/images/eatscape.webp",
+        "/images/phostel.webp",
+    ].forEach((src) => {
+        const img = new Image();
+        img.src = src;
+    });
+});
 </script>
 
 <template>
