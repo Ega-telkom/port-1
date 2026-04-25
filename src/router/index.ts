ee7421fd8ca9ff07a4fd7c55407bc2e35d8f260a
Ega-telkom <541241041@student.smktelkom-pwt.sch.id>
2026-04-25 15:17:26 +0700

    i18n: add to title

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,16 +8,19 @@ const routes: RouteRecordRaw[] = [
   {
     path: "/:locale(en|id)?",
     name: "home",
+    meta: { title: 'home.title' },
     component: Home,
   },
   {
     path: "/:locale(en|id)?/project",
     name: "project",
+    meta: { title: 'project.title' },
     component: Project,
   },
   {
     path: "/:locale(en|id)?/:pathMatch(.*)*",
     name: "not-found",
+    meta: { title: '404.title' },
     component: NotFound,
   },
 ];
@@ -30,6 +33,8 @@ const router = createRouter({
 router.beforeEach((to) => {
   const locale = (to.params.locale as "en" | "id") || "en"
   i18n.global.locale.value = locale
+  const { t } = i18n.global
+  document.title = `Ega—${t(to.meta.title as string)}`
 })
 
 export default router;
